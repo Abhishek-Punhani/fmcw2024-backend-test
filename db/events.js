69483c955c6ad79974c1fac234cc3edc40e94a8d
Abhishek <punhaniabhishek17@gmail.com>
2024-08-30 01:45:20 +0530

    Added Event router

---
+++ db/events.js
@@ -0,0 +1,125 @@
+module.exports.jsonString = `{
+  "Photog": {
+    "Snapchase": {
+      "id": "fmc_1",
+      "price": 0,
+      "name": "Snapchase"
+    },
+    "Portfolio in 24 Hours": {
+      "id": "fmc_2",
+      "price": 0,
+      "name": "Portfolio in 24 Hours"
+    },
+    "PhotoArt": {
+      "id": "fmc_3",
+      "price": 0,
+      "name": "PhotoArt"
+    },
+    "Instameet": {
+      "id": "fmc_4",
+      "price": 0,
+      "name": "Instameet"
+    },
+    "Infocus": {
+      "id": "fmc_5",
+      "price": 0,
+      "name": "Infocus"
+    }
+  },
+  "Outreach": {
+    "Vlogoholic": {
+      "id": "fmc_6",
+      "price": 0,
+      "name": "Vlogoholic"
+    },
+    "Rantlt": {
+      "id": "fmc_7",
+      "price": 0,
+      "name": "Rantlt"
+    },
+    "Documentary": {
+      "id": "fmc_8",
+      "price": 0,
+      "name": "Documentary"
+    },
+    "Admad": {
+      "id": "fmc_9",
+      "price": 0,
+      "name": "Admad"
+    }
+  },
+  "Media": {
+    "WriterBlock": {
+      "id": "fmc_10",
+      "price": 0,
+      "name": "WriterBlock"
+    },
+    "SFS": {
+      "id": "fmc_11",
+      "price": 0,
+      "name": "SFS"
+    },
+    "Laughing Gazette": {
+      "id": "fmc_12",
+      "price": 0,
+      "name": "Laughing Gazette"
+    },
+    "Infinite Tales": {
+      "id": "fmc_13",
+      "price": 0,
+      "name": "Infinite Tales"
+    }
+  },
+  "Design": {
+    "Swap The Canvas": {
+      "id": "fmc_14",
+      "price": 0,
+      "name": "Swap The Canvas"
+    },
+    "Relnvent": {
+      "id": "fmc_15",
+      "price": 0,
+      "name": "Relnvent"
+    },
+    "Design The Tunes": {
+      "id": "fmc_16",
+      "price": 0,
+      "name": "Design The Tunes"
+    },
+    "Design-a-thon": {
+      "id": "fmc_17",
+      "price": 0,
+      "name": "Design-a-thon"
+    }
+  },
+  "Cine": {
+    "Make it Short": {
+      "id": "fmc_18",
+      "price": 0,
+      "name": "Make it Short"
+    },
+    "Directors Cut": {
+      "id": "fmc_19",
+      "price": 0,
+      "name": "Directors Cut"
+    },
+    "Digital Dastaan": {
+      "id": "fmc_20",
+      "price": 0,
+      "name": "Digital Dastaan"
+    },
+    "Broll": {
+      "id": "fmc_21",
+      "price": 0,
+      "name": "Broll"
+    }
+  },
+  "Animation": {
+    "Capture The Imagination": {
+      "id": "fmc_21",
+      "price": 0,
+      "name": "Capture The Imagination"
+    }
+  }
+}
+`;
