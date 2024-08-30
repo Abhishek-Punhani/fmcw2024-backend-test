aa20b20a1fbe9c054c1f8e299b3b05e39d3614c5
the-dg04 <devgup04@gmail.com>
2024-08-30 21:38:18 +0530

    dfsfdsfdsc

--- controllers/user/checkout.controller.js
+++ controllers/user/checkout.controller.js
@@ -3,24 +3,28 @@ const JSON = require("JSON");
 
 module.exports = async (req, res) => {
   if (res.auth) {
-    const { events } = req.body;
+    // const { events } = req.body;
     const collection = await db.collection("users");
+    const info = await collection.findOne(
+      { email: res.email },
+      { projection: { cart: 1, registered: 1 } }
+    );
     const registered = JSON.parse(
       "[" +
-        (
-          await collection.findOne(
-            { email: res.email },
-            { projection: { registered: 1 } }
-          )
-        ).registered +
+        info.registered +
+        "]"
+    );
+    const cart = JSON.parse(
+      "[" +
+        info.cart +
         "]"
     );
     const result = await collection.updateOne(
       { email: res.email },
-      { $set: { registered: registered.concat(events) } }
+      { $set: { registered: registered.concat(cart),cart:[] } }
     );
     res.json(result).status(200);
   } else {
-    res.json({'message':'Forbidden'}).status(400);
+    res.json({ message: "Forbidden" }).status(400);
   }
 };
