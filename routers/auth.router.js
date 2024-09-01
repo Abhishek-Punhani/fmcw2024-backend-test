ed569e211f518dc3d631113d090286b3852125b4
the-dg04 <devgup04@gmail.com>
2024-09-01 16:32:50 +0530

    sadksaldija

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,21 +1,31 @@
-const express = require("express")
-const router = express.Router()
-const passport = require('passport')
-const jwt = require("jsonwebtoken")
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 const db = require("./../db/conn.js");
-router.get("/",(req,res)=>{res.send("auth")})
-
-router.get('/login', passport.authenticate('google', { session: false, scope: ['profile', 'email'] }))
-
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/'}),async (req, res) => {
-    const collection = await db.collection('users')
-    const email = jwt.verify(req.user.token,process.env.JWT_SECRET)['email'][0]['value'];
-    const result = await collection.findOne({email:email})
-    if(result) res.json({'authToken':req.user.token,'newUser':false}).status(200)
-    else res.json({'authToken':req.user.token,'newUser':true}).status(200)
+router.get("/", (req, res) => {
+  res.send("auth");
 });
 
-router.get('/logout',(req,res)=>{
-    res.json({'message':'logged out'}).status(200);
-})
-module.exports=router
+// router.get('/login', passport.authenticate('google', { session: false, scope: ['profile', 'email'] }))
+
+// router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/'}),async (req, res) => {
+//     const collection = await db.collection('users')
+//     const email = jwt.verify(req.user.token,process.env.JWT_SECRET)['email'][0]['value'];
+//     const result = await collection.findOne({email:email})
+//     if(result) res.json({'authToken':req.user.token,'newUser':false}).status(200)
+//     else res.json({'authToken':req.user.token,'newUser':true}).status(200)
+// });
+
+router.get("/login",async (req, res) => {
+  const { authToken } = req.body;
+  const collection = await db.collection('users')
+    const email = jwt.verify(authToken,process.env.JWT_SECRET)['email'][0]['value'];
+    const result = await collection.findOne({email:email})
+     res.json({'newUser':result?false:true}).status(200)
+});
+
+// router.get("/logout", (req, res) => {
+//   res.json({ message: "logged out" }).status(200);
+// });
+module.exports = router;
