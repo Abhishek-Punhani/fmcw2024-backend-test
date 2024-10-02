db73ffe6ed2d72abec63102b9930f0f180231ad0
the-dg04 <devgup04@gmail.com>
2024-10-02 12:46:09 +0530

    added event names

--- routers/payment.router.js
+++ routers/payment.router.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const getEventsById=require("./../utils/events/events.js")
 const router = express.Router();
 const db = require("./../db/conn.js");
 
 router.get("/", async (req, res) => {
   const collection = await db.collection("payments");
   const result = await collection.find().toArray();
+
+  result.map((e)=>{e['purchased_events']=getEventsById(e['purchased_events'])})
   res.json(result).status(200);
 });
 
